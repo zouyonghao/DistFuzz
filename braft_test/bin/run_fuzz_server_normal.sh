f31b7ad12faf0c17fdb1f0dafc4a89102e775593
zouyonghao <yonghaoz1994@gmail.com>
2021-03-16 08:30:38 +0000

    Use one-line env var for different node process

--- braft_test/bin/run_fuzz_server_normal.sh
+++ braft_test/bin/run_fuzz_server_normal.sh
@@ -1,8 +1,8 @@
 bin_path=/home/zyh/braft/example/atomic
 
-"$bin_path"/atomic_server -reuse_addr -election_timeout_ms=700 -port=8300 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data1" > log1 2>&1 &
-"$bin_path"./atomic_server -reuse_addr -election_timeout_ms=600 -port=8301 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data2" > log2 2>&1 &
-"$bin_path"./atomic_server -reuse_addr -election_timeout_ms=500 -port=8302 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data3" > log3 2>&1 &
+NODE_NAME=node1 "$bin_path"/atomic_server -reuse_addr -port=8300 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data1" > log1 2>&1 &
+NODE_NAME=node2 "$bin_path"/atomic_server -reuse_addr -port=8301 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data2" > log2 2>&1 &
+NODE_NAME=node3 "$bin_path"/atomic_server -reuse_addr -port=8302 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data3" > log3 2>&1 &
 
 # ./atomic_server -port=8303 -conf="127.0.1.1:8303:0,127.0.1.1:8304:0,127.0.1.1:8305:0," > log3 2>&1 &
 # ./atomic_server -port=8303 -conf="127.0.1.1:8304:0,127.0.1.1:8305:0,127.0.1.1:8303:0," -data_path="./data3" > log3 2>&1 &
