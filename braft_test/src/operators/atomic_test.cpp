e13ea20f91f385b7092b2ea8df9c17dd08457699
zouyonghao <yonghaoz1994@gmail.com>
2021-04-06 03:20:35 +0000

    Support operation log checker!

--- braft_test/src/operators/atomic_test.cpp
+++ braft_test/src/operators/atomic_test.cpp
@@ -1,33 +1,122 @@
 #include <dst_default_operator.hpp>
+#include <dst_event.h>
 #include <dst_random.h>
 
+#include <limits>
+#include <map>
+#include <sstream>
+
+#include <boost/process.hpp>
+
 static const std::string ATOMIC_TEST_PATH = "/home/zyh/braft/example/atomic";
+
+static std::map<std::string, std::string> OP_MAP = {
+    {"get", "read"}, {"set", "write"}, {"cas", "cas"}};
 
 class AtomicTestOperator : public Operator
 {
 private:
+    std::string atomic_op;
     std::string unfinished_command;
     std::vector<std::string> need_random_parameters;
+
+    std::string convert_value_vector(std::vector<std::string> &values)
+    {
+        switch (values.size())
+        {
+        case 0:
+            return "nil";
+        case 1:
+            return values[0];
+        default:
+        {
+            std::string result = "[";
+            for (auto &t : values)
+            {
+                result += t + " ";
+            }
+            result += "]";
+            return result;
+        }
+        }
+    }
+
+    std::string get_thread_id()
+    {
+        std::stringstream ss;
+        ss << std::this_thread::get_id();
+        return std::to_string(std::stoull(ss.str()) % INT_MAX);
+    }
+
+    std::string get_invoke_record(std::vector<std::string> &values)
+    {
+        return std::string("{:process " + get_thread_id() +
+                           ", :type :invoke, :f :" + OP_MAP[atomic_op] +
+                           ", :value " + convert_value_vector(values) + "}");
+    }
+
+    std::string get_result_record(std::vector<std::string> &values, int result,
+                                  std::string &last_output)
+    {
+        std::string value = convert_value_vector(values);
+        if (atomic_op == "get")
+        {
+            try
+            {
+                value = std::to_string(std::stoi(last_output));
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+        }
+        return std::string("{:process " + get_thread_id() + ", :type " +
+                           std::string(result == 0 ? ":ok" : ":fail") +
+                           ", :f :" + OP_MAP[atomic_op] + ", :value " + value +
+                           "}");
+    }
 
 public:
     AtomicTestOperator(std::string _conf, std::string _atomic_op,
                        std::vector<std::string> _need_random_parameters)
-        : need_random_parameters(_need_random_parameters)
+        : atomic_op(_atomic_op), need_random_parameters(_need_random_parameters)
     {
-        unfinished_command = "timeout 2 " + ATOMIC_TEST_PATH + "/atomic_test -conf=" + _conf +
+
+        // TODO currently we only support one atomic_id (aka. one register)
+        unfinished_command = "timeout 2 " + ATOMIC_TEST_PATH +
+                             "/atomic_test -conf=" + _conf +
                              " -atomic_op=" + _atomic_op;
     }
 
     virtual bool _do()
     {
         std::string command = unfinished_command;
+        std::vector<std::string> values;
         for (auto &i : need_random_parameters)
         {
-            command += " -" + i + "=" +
-                       std::to_string(__dst_get_random_uint32()) + " ";
+            std::string random = std::to_string(__dst_get_random_uint32());
+            command += " -" + i + "=" + random + " ";
+            values.push_back(random);
         }
+        __dst_event_record(get_invoke_record(values).c_str());
         std::cout << command << "\n";
-        return std::system(command.c_str()) == 0;
+        // int result = std::system(command.c_str());
+
+        boost::process::ipstream pipe_stream;
+        boost::process::child c(command, boost::process::std_out > pipe_stream);
+        c.wait();
+        int result = c.exit_code();
+
+        std::string tmp;
+        std::string last_output;
+        while (pipe_stream && std::getline(pipe_stream, tmp) && !tmp.empty())
+        {
+            last_output = tmp;
+        }
+
+        __dst_event_record(
+            get_result_record(values, result, last_output).c_str());
+        return result;
     }
 };
 
@@ -36,12 +125,12 @@ public:
 REGISTER_OPERATOR(AtomicTestGet,
                   new AtomicTestOperator(
                       "127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0,",
-                      "get", {"atomic_id"}));
+                      "get", {}));
 REGISTER_OPERATOR(AtomicTestSet,
                   new AtomicTestOperator(
                       "127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0,",
-                      "set", {"atomic_id", "atomic_val"}));
+                      "set", {"atomic_val"}));
 REGISTER_OPERATOR(AtomicTestCas,
                   new AtomicTestOperator(
                       "127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0,",
-                      "get", {"atomic_id", "atomic_val", "atomic_new_val"}));
+                      "cas", {"atomic_val", "atomic_new_val"}));
--- event_control/src/main.cpp
+++ event_control/src/main.cpp
@@ -1,3 +1,7 @@
+#include <cstring>
+#include <signal.h>
+
+#include <fstream>
 #include <iostream>
 
 #include <boost/interprocess/ipc/message_queue.hpp>
@@ -6,31 +10,61 @@
 
 using namespace boost::interprocess;
 
+bool running = true;
+
+void kill_handler(int signal)
+{
+    std::cout << "receive sigkill, closing..."
+              << "\n";
+    running = false;
+}
+
 int main(int argc, char *argv[])
 {
+    signal(SIGKILL, kill_handler);
+    signal(SIGTERM, kill_handler);
+    std::ofstream out("operation_log", std::ios::out | std::ios::binary);
+    out << "[";
     try
     {
         message_queue::remove(EventConstant::EVENT_QUEUE_ID);
         message_queue mq(create_only, EventConstant::EVENT_QUEUE_ID,
                          EventConstant::MAX_QUEUE_NUMBER,
                          EventConstant::MAX_QUEUE_MESSAGE_SIZE);
-        while (true)
+        while (running)
         {
             std::string s;
             message_queue::size_type message_size;
             uint32_t message_priority;
             s.resize(EventConstant::MAX_QUEUE_MESSAGE_SIZE);
 
-            mq.receive(&s[0], s.size(), message_size, message_priority);
+            if (!mq.try_receive(&s[0], s.size(), message_size, message_priority))
+            {
+                sleep(1);
+                continue;
+            }
             s.resize(message_size);
-
-            std::cout << "Receive message :" << s << "\n";
+            std::string::size_type n =
+                s.find(std::string(EventConstant::NEED_RECORD_MESSAGE_PREFIX));
+            if (n != std::string::npos)
+            {
+                std::string content = s.substr(
+                    n + strlen(EventConstant::NEED_RECORD_MESSAGE_PREFIX));
+                std::cout << "Receive need record message: " << content << "\n";
+                out << content << ",\n";
+            }
+            else
+            {
+                std::cout << "Receive message :" << s << "\n";
+            }
         }
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
+    out << "]";
+    out.close();
 
     message_queue::remove(EventConstant::EVENT_QUEUE_ID);
 
