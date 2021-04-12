1d4c57bd11c236aa84513da8061820284e2beb7b
zouyonghao <yonghaoz1994@gmail.com>
2021-04-12 14:02:37 +0000

    Add more log checkers

--- braft_test/bin/backup_test_case.sh
+++ braft_test/bin/backup_test_case.sh
@@ -12,4 +12,13 @@ mv random.txt ./test_cases/$1
 mv operation_log ./test_cases/$1
 
 # checking the operation log!
-./check.sh ${PWD}/test_cases/$1/operation_log > ${PWD}/test_cases/$1/check_log 2>&1 &
+{ \
+    ./check.sh ${PWD}/test_cases/$1/operation_log > ${PWD}/test_cases/$1/check_log 2>&1; \
+    if grep -q ^E ./test_cases/$1/log*; then echo "Find errors!"; \
+    elif grep -q "ERROR: AddressSanitizer"; then echo "Find ASan errors!"; \
+    elif grep -q true ./test_cases/$1/check_log; then echo "No errors, deleting logs..."; \
+        rm -rf ./test_cases/$1; \
+    else \
+        echo "Find operation log errors!"; \
+    fi \
+} &
