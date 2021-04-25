b52ff678182148fc547a0b970a992d874d420917
zouyonghao <yonghaoz1994@gmail.com>
2021-04-25 13:46:46 +0000

    more test cases

--- rethinkdb_test/bin/start.sh
+++ rethinkdb_test/bin/start.sh
@@ -12,7 +12,10 @@ ${RETHINKDB_BUILD_PATH}/rethinkdb --bind 127.0.0.1 --cluster-port 5001 \
         --driver-port 4001 --http-port 8081 -d ./rethinkdb_data1 \
         -j 127.0.0.1:5000 -j 127.0.0.1:5002 >> log1 2>&1 &
 # sleep 1
-${RETHINKDB_BUILD_PATH}/rethinkdb --bind 127.0.0.1 --cluster-port 5002 \
+../../build/proxy_server/proxy_server ./proxy-config.json > log_proxy 2>&1 &
+../../build/event_control/event_control > log_event_control 2>&1 &
+
+${RETHINKDB_BUILD_PATH}/rethinkdb --bind 127.0.0.1 --cluster-port 6002 \
         --driver-port 4002 --http-port 8082 -d ./rethinkdb_data2 \
-        -j 127.0.0.1:5000 \
-        -j 127.0.0.1:5001 >> log2 2>&1 &
+        -j 127.0.0.1:6000 \
+        -j 127.0.0.1:6001 >> log2 2>&1 &
