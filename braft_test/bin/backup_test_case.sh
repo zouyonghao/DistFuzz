2e8361e98dff58dc683c1a84b8e4d5ad92560e04
zouyonghao <yonghaoz1994@gmail.com>
2021-05-11 12:26:20 +0000

    Fix backup_test

--- braft_test/bin/backup_test_case.sh
+++ braft_test/bin/backup_test_case.sh
@@ -23,7 +23,7 @@ mv operation_log ./test_cases/$1
     ./check.sh ${PWD}/test_cases/$1/operation_log >${PWD}/test_cases/$1/check_log 2>&1
     if grep -q ^E ./test_cases/$1/log*; then
         echo "Find errors!"
-    elif [ $CHECK_RAFT_LOG_ERROR == 1 ]; then
+    elif [[ $CHECK_RAFT_LOG_ERROR == 1 ]]; then
         echo "Check raft log failed!"
     elif grep -q "ERROR: AddressSanitizer" ./test_cases/$1/log[0-2]; then
         echo "Find ASan errors!"
