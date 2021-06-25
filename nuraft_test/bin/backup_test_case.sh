eb00f38b75af4b549d22c91b88a5bd01c3e15fe2
zouyonghao <yonghaoz1994@gmail.com>
2021-06-25 08:33:22 +0000

    update Nuraft test scripts

--- nuraft_test/bin/backup_test_case.sh
+++ nuraft_test/bin/backup_test_case.sh
@@ -16,8 +16,10 @@ mv operation_log ./test_cases/$1
 # checking the operation log!
 {
     ./check.sh ${PWD}/test_cases/$1/operation_log >${PWD}/test_cases/$1/check_log 2>&1
-    if grep -q 'FATL' ./test_cases/$1/log*; then
+    if grep 'FATL' ./test_cases/$1/log* | grep -v "cannot find leader" | grep -v "too many pre-vote rejections"; then
         echo "Find errors!"
+    elif grep -q "AddressSanitizer" ./test_cases/$1/*.log; then
+        echo "Find ASan errors!"
     elif grep -q "ERROR: AddressSanitizer" ./test_cases/$1/srv[1-3].log; then
         echo "Find ASan errors!"
     elif grep -q true ./test_cases/$1/check_log; then
---
+++ nuraft_test/report.md
@@ -0,0 +1,2 @@
+1. Crtl-D input issue for client
+    https://github.com/eBay/NuRaft/pull/211/files
