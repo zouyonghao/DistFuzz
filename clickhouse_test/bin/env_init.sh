5b9668514fb8340620d984ffaade616d95ee2ef9
zouyonghao <yonghaoz1994@gmail.com>
2021-07-12 13:37:02 +0000

    automatic check server status

--- clickhouse_test/bin/env_init.sh
+++ clickhouse_test/bin/env_init.sh
@@ -1,4 +1,7 @@
-sleep 10
+
+for (( ; ; )) do /home/zyh/ClickHouse/build/programs/clickhouse-client -c config0.xml -q "select 1" && break; done
+for (( ; ; )) do /home/zyh/ClickHouse/build/programs/clickhouse-client -c config1.xml -q "select 1" && break; done
+for (( ; ; )) do /home/zyh/ClickHouse/build/programs/clickhouse-client -c config2_for_proxy.xml -q "select 1" && break; done
 
 /home/zyh/ClickHouse/build/programs/clickhouse-client -c config0.xml -q "CREATE DATABASE test"
 /home/zyh/ClickHouse/build/programs/clickhouse-client -c config0.xml -q "CREATE TABLE test.test (key UInt32, v UInt32) ENGINE=ReplicatedMergeTree('/clickhouse/tables/{shard}/test', '{replica}') ORDER BY key"
