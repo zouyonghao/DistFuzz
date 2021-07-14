2e4a43699a3f386799bb388f57c14c725f7e4c1e
zouyonghao <yonghaoz1994@gmail.com>
2021-07-14 12:05:23 +0000

    update dep scripts

--- clickhouse_test/bin/restore_checkpoint_tmpfs.sh
+++ clickhouse_test/bin/restore_checkpoint_tmpfs.sh
@@ -2,6 +2,6 @@ set -x
 
 cp -r run_backup_tmpfs/* run
 
-sudo /home/zyh/criu-criu-dev/scripts/criu-ns restore --images-dir checkpoint_folder0_tmpfs -v4 -o rst.log --tcp-established &
-sudo /home/zyh/criu-criu-dev/scripts/criu-ns restore --images-dir checkpoint_folder1_tmpfs -v4 -o rst.log --tcp-established &
-sudo /home/zyh/criu-criu-dev/scripts/criu-ns restore --images-dir checkpoint_folder2_tmpfs -v4 -o rst.log --tcp-established &
+sudo /home/zyh/criu/scripts/criu-ns restore --images-dir checkpoint_folder0_tmpfs -v4 -o rst.log --tcp-established &
+sudo /home/zyh/criu/scripts/criu-ns restore --images-dir checkpoint_folder1_tmpfs -v4 -o rst.log --tcp-established &
+sudo /home/zyh/criu/scripts/criu-ns restore --images-dir checkpoint_folder2_tmpfs -v4 -o rst.log --tcp-established &
--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,5 @@
-sudo apt install \
+set -e
+sudo apt-get install \
     libboost-all-dev \
     rapidjson-dev \
     clang-9 \
@@ -11,17 +12,39 @@ sudo apt install \
     libleveldb-dev \
     autoconf \
     libbsd-dev \
-    redis
+    redis -y > /dev/null && echo "apt-get dependencies success!"
 
 # install aerospike client
-wget https://download.aerospike.com/artifacts/aerospike-client-c/5.2.0/aerospike-client-c-5.2.0.ubuntu18.04.x86_64.tgz
-tar xvf aerospike-client-c-5.2.0.ubuntu18.04.x86_64.tgz
-cd aerospike-client-c-5.2.0.ubuntu18.04.x86_64
-sudo dpkg -i aerospike-client-c-devel-5.2.0.ubuntu18.04.x86_64.deb
-cd ..
+if ls /usr/include/aerospike > /dev/null; then
+    echo "aerospike client is installed, skip."
+else
+    echo "installing aerospike client..."
+    wget https://download.aerospike.com/artifacts/aerospike-client-c/5.2.0/aerospike-client-c-5.2.0.ubuntu18.04.x86_64.tgz
+    tar xvf aerospike-client-c-5.2.0.ubuntu18.04.x86_64.tgz
+    cd aerospike-client-c-5.2.0.ubuntu18.04.x86_64
+    sudo dpkg -i aerospike-client-c-devel-5.2.0.ubuntu18.04.x86_64.deb
+    cd ..
+    echo "install aerospike client lib success!"
+fi
 
 # install java, lein
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-sudo mv lein /usr/bin/
-sudo chmod +x /usr/bin/lein
-sudo apt install default-jdk
+if lein version > /dev/null; then
+    echo "leiningen is installed, skip."
+else
+    echo "installing leiningen and jdk..."
+    wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+    sudo mv lein /usr/bin/
+    sudo chmod +x /usr/bin/lein
+    sudo apt install default-jdk
+    echo "install lein success!"
+fi
+
+cd
+if ls criu > /dev/null; then
+    echo "it seems you have installed criu, skip."
+else
+    echo "installing criu..."
+    git clone http://166.111.139.139:7800/zyh/criu.git
+    cd criu
+    make && echo "install criu success!"
+fi
