3b4026ca3abdbe29e7dc47bd4d84ed168365166e
zouyonghao <yonghaoz1994@gmail.com>
2021-07-15 13:52:38 +0000

    redirect error message to log_test_err

--- clickhouse_test/bin/run_random_test.sh
+++ clickhouse_test/bin/run_random_test.sh
@@ -1,4 +1,4 @@
 for (( ; ; )); do
     ./generate_50k_bytes.sh init_random.txt
-    /home/zyh/distributed-system-test/build/clickhouse_test/clickhouse_test_main init_random.txt >log_test 2>&1
+    /home/zyh/distributed-system-test/build/clickhouse_test/clickhouse_test_main init_random.txt >log_test 2>log_test_err
 done
