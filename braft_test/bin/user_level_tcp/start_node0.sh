1030e5b8878cc6e3a87711cd09fa3f207af8f451
zouyonghao <yonghaoz1994@gmail.com>
2021-08-03 13:10:17 +0000

    Fix conf

--- braft_test/bin/user_level_tcp/start_node0.sh
+++ braft_test/bin/user_level_tcp/start_node0.sh
@@ -7,4 +7,4 @@ export DPDK_IP_MASK=24
 export DPDK_VNIC="--vdev=virtio_user0,path=/dev/vhost-net,queue_size=1024,iface=veth0"
 # export DPDK_PARAMS="--proc-type=primary"
 export DPDK_PARAMS="--file-prefix=node0"
-LD_PRELOAD=/home/zyh/tldk/libtldk.so NODE_NAME=node0 "$bin_path"/atomic_server -reuse_addr -reuse_port -ip=10.0.1.1 -port=8300 -conf="10.0.1.1:8300:0,10.0.1.2:8300:0,10.0.1.1:8300:0," -data_path="./data0" > log0 2>&1 &
+LD_PRELOAD=/home/zyh/tldk/libtldk.so NODE_NAME=node0 "$bin_path"/atomic_server -reuse_addr -reuse_port -ip=10.0.1.1 -port=8300 -conf="10.0.1.1:8300:0,10.0.1.2:8300:0,10.0.1.3:8300:0," -data_path="./data0" > log0 2>&1 &
