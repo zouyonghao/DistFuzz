3d82f774657fce1b9c1d19ef606921a7f7eb1641
zouyonghao <yonghaoz1994@gmail.com>
2021-09-16 03:33:59 +0000

    Fix server ip

--- c-raft_test/bin/start_server2.sh
+++ c-raft_test/bin/start_server2.sh
@@ -9,4 +9,4 @@ CRAFT_SERVER_PATH=/home/zyh/raft/example
 # $CRAFT_SERVER_PATH/server ./c-raft_data0 1 9000 9000 9001 9002 &
 # $CRAFT_SERVER_PATH/server ./c-raft_data1 2 9001 9000 9001 9002 &
 LD_PRELOAD=/home/zyh/distributed-system-test/build/preload_module/libdst_preload.so \
-__DST_ENV_RANDOM_FILE__=random_node2.txt $CRAFT_SERVER_PATH/server ./c-raft_data2 3 9003 9004 9005 9003 >> log2 2>&1 &
+__DST_ENV_RANDOM_FILE__=random_node2.txt $CRAFT_SERVER_PATH/server ./c-raft_data2 3 9002 9000 9001 9002 >> log2 2>&1 &
