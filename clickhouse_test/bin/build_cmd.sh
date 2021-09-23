cf559cf42e75bd01a3f61539d6c93f674daedd40
zouyonghao <yonghaoz1994@gmail.com>
2021-09-23 12:43:06 +0000

    Only build keeper

--- clickhouse_test/bin/build_cmd.sh
+++ clickhouse_test/bin/build_cmd.sh
@@ -9,14 +9,14 @@
 cp /home/zyh/distributed-system-test/clickhouse_test/bin/compiler-config.json /tmp/
 
 export CC=/home/zyh/distributed-system-test/build/fuzz/default_compiler CXX=/home/zyh/distributed-system-test/build/fuzz/default_compiler++
-cmake ..  \
-        -DCMAKE_BUILD_TYPE=Debug     \
-        -DENABLE_CLICKHOUSE_ALL=OFF     \
-        -DENABLE_CLICKHOUSE_SERVER=ON     \
-        -DENABLE_CLICKHOUSE_CLIENT=ON     \
-        -DENABLE_LIBRARIES=OFF     \
-        -DUSE_UNWIND=ON     \
-        -DENABLE_UTILS=OFF     \
+cmake .. \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DENABLE_CLICKHOUSE_ALL=OFF \
+        -DENABLE_CLICKHOUSE_SERVER=OFF \
+        -DENABLE_CLICKHOUSE_CLIENT=OFF \
+        -DENABLE_LIBRARIES=OFF \
+        -DUSE_UNWIND=ON \
+        -DENABLE_UTILS=OFF \
         -DENABLE_TESTS=OFF -DSANITIZE=address \
         -DENABLE_NURAFT=ON \
         -DENABLE_CLICKHOUSE_KEEPER=ON
