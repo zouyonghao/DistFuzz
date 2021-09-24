c1973c09e96c58d16246b828e67593f430eb1fc0
zouyonghao <yonghaoz1994@gmail.com>
2021-09-24 12:47:57 +0000

    reduce election timeout

--- braft_test/bin/start_node1.sh
+++ braft_test/bin/start_node1.sh
@@ -1,4 +1,4 @@
 bin_path=/home/zyh/braft/example/atomic
 
 LD_PRELOAD=/home/zyh/distributed-system-test/build/preload_module/libdst_preload.so \
-__DST_ENV_RANDOM_FILE__=random_node1.txt NODE_NAME=node1 "$bin_path"/atomic_server -reuse_addr -reuse_port -port=8301 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data1" >> log1 2>&1 &
+__DST_ENV_RANDOM_FILE__=random_node1.txt NODE_NAME=node1 "$bin_path"/atomic_server -reuse_addr -ip=127.0.1.1 -reuse_port -port=8301 -election_timeout_ms=100 -conf="127.0.1.1:8300:0,127.0.1.1:8301:0,127.0.1.1:8302:0," -data_path="./data1" >> log1 2>&1 &
