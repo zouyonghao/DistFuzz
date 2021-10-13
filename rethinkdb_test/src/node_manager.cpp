5bc7f5a9546d7813a5d75807b15327c0b1c589c4
zouyonghao <zouyonghao1994@163.com>
2021-10-13 17:32:46 +0800

    Suppor rethinkdb multi nodes.

---
+++ rethinkdb_test/src/node_manager.cpp
@@ -0,0 +1,4 @@
+#include <rethinkdb_configuration_generator.hpp>
+#include <dst_node_manager.hpp>
+
+REGISTER_NODE_MANAGER(REDISRAFT_NODE_MANAGER, new NodeManager(new RethinkdbConfigurationGenerator()));
--- rethinkdb_test/src/operators/rethinkdb_client_test.cpp
+++ rethinkdb_test/src/operators/rethinkdb_client_test.cpp
@@ -1,22 +1,8 @@
-// #include <operator/dst_default_client_operator.hpp>
+#include <operator/dst_default_client_operator.hpp>
+#include <rethinkdb_configuration_generator.hpp>
 
-// static const std::string RETHINKDB_CLIENT =
-//     "/usr/bin/java -jar "
-//     "/home/zyh/distributed-system-test/rethinkdb_test/client/target/"
-//     "rethinkdb-test-client-1.0-SNAPSHOT-jar-with-dependencies.jar";
-// static const std::string UNFINISHED_COMMAND =
-//     "timeout 5 " + RETHINKDB_CLIENT + " ";
+static RethinkdbClientConfigurationGenerator *rccg = new RethinkdbClientConfigurationGenerator();
 
-// REGISTER_NORMAL_OPERATOR(RethinkDBGet,
-//                          new DefaultClientOperator(UNFINISHED_COMMAND + "get ",
-//                                                    "read", {}));
-// REGISTER_NORMAL_OPERATOR(RethinkDBSet,
-//                          new DefaultClientOperator(UNFINISHED_COMMAND + "set ",
-//                                                    "write", {" "}));
-// REGISTER_NORMAL_OPERATOR(RethinkDBCas,
-//                          new DefaultClientOperator(UNFINISHED_COMMAND + "cas ",
-//                                                    "cas", {" ", " "}));
-// REGISTER_NORMAL_OPERATOR(Init, new DefaultClientOperator("timeout 30 " +
-//                                                              RETHINKDB_CLIENT +
-//                                                              " " + "get ",
-//                                                          "read", {}));
+REGISTER_NORMAL_OPERATOR(RethinkdbGet, new DefaultClientOperator(OP_READ, rccg));
+REGISTER_NORMAL_OPERATOR(RethinkdbSet, new DefaultClientOperator(OP_WRITE, rccg));
+REGISTER_NORMAL_OPERATOR(RethinkdbCas, new DefaultClientOperator(OP_CAS, rccg));
