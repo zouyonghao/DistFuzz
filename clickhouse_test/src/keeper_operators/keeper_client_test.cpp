e269feb0d50a333c299bd0adba8c0d3d51441016
zouyonghao <zouyonghao1994@163.com>
2021-10-27 16:55:50 +0800

    Fix keeper init operator

--- clickhouse_test/src/keeper_operators/keeper_client_test.cpp
+++ clickhouse_test/src/keeper_operators/keeper_client_test.cpp
@@ -5,9 +5,33 @@ static ClientConfigurationGenerator *keeper_client_configuration_generator = new
 /** The KeeperInitOperator will try to set until the read is success */
 class KeeperInitOperator : public DefaultClientOperator
 {
+    class InitConfigurationGenerator : public ClientConfigurationGenerator
+    {
+    public:
+        std::string get_configure_string(OP_NAME op_name, uint32_t node_count, ...)
+        {
+            std::string configure_string = "timeout 2 /usr/share/zookeeper/bin/zkCli.sh -server ";
+            for (uint32_t i = 0; i < node_count; i++)
+            {
+                configure_string += IP ":" + std::to_string(BASE_ZK_PORT + i);
+                if (i < node_count - 1)
+                {
+                    configure_string += ",";
+                }
+            }
+            configure_string += " ";
+            va_list random_nums;
+            va_start(random_nums, node_count);
+            uint32_t value = va_arg(random_nums, uint32_t);
+            configure_string += "create /a " + std::to_string(value);
+            va_end(random_nums);
+            return configure_string;
+        }
+    };
+
 public:
     const int MAX_TRY_COUNT = 1000;
-    KeeperInitOperator() : DefaultClientOperator(OP_WRITE, keeper_client_configuration_generator) {}
+    KeeperInitOperator() : DefaultClientOperator(OP_WRITE, new InitConfigurationGenerator) {}
 
     bool _do() override
     {
