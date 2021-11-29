0dc0ad08d55698a2e53ac4907c5f4936ee3ff81d
zouyonghao <zouyonghao1994@163.com>
2021-11-29 11:32:56 +0800

    Add init for redisraft

--- redisraft_test/src/operators/redisraft_client.cpp
+++ redisraft_test/src/operators/redisraft_client.cpp
@@ -9,6 +9,7 @@ REGISTER_NORMAL_OPERATOR(RedisraftTestSet, new DefaultClientOperator(OP_WRITE, r
 class RedisraftInitOperator : public NormalOperator
 {
 public:
+    int MAX_TRY_COUNT = 5;
     bool _do() override
     {
         sleep(1);
@@ -20,6 +21,30 @@ public:
             std::cerr << "init command is " << command << "\n";
             system(command.c_str());
         }
+
+        int count = 0;
+        std::string command = "timeout 3 bash /home/zyh/distributed-system-test/redisraft_test/bin/run_client.sh set 0";
+        for (; count < MAX_TRY_COUNT; count++)
+        {
+            boost::process::child c(command);
+            c.wait();
+            if (c.exit_code() == 0)
+            {
+                break;
+            }
+        }
+        if (count >= MAX_TRY_COUNT)
+        {
+            std::cerr << "Run init failed!\n";
+            return false;
+        }
+        int random_thread_id = random() % INT_MAX;
+        std::string invoke_record_string =
+            "{:process " + std::to_string(random_thread_id) + ", :type :invoke, :f :write, :value 0}";
+        __dst_event_record(invoke_record_string.c_str());
+        std::string result_record_string =
+            "{:process " + std::to_string(random_thread_id) + ", :type :ok, :f :write, :value 0}";
+        __dst_event_record(result_record_string.c_str());
         return true;
     }
 };
