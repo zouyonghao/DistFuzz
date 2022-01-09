9a8ccf32679adf7b41380e1acb7bff6b41d9b70a
zouyonghao <zouyonghao1994@163.com>
2022-01-09 14:39:16 +0800

    etcd boot_time_test supported.

---
+++ etcd_test/bin/boot_time_test/ensure_nodes.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -x
+
+for ((;;)); do
+    /home/zyh/etcd/bin/etcdctl \
+        --endpoints=127.0.1.1:2380,127.0.1.1:2381,127.0.1.1:2382,127.0.1.1:2383,127.0.1.1:2384 \
+        put a 123 && break;
+done
---
+++ etcd_test/bin/boot_time_test/env_init_checkpoint.sh
@@ -0,0 +1,22 @@
+rm checkpoint_folder -rf
+mkdir checkpoint_folder
+
+./run_fuzz_server_normal.sh
+./ensure_nodes.sh
+
+# cp -r data data_backup
+
+for i in 0 1 2 3 4; do
+    mkdir checkpoint_folder/$i
+    PID=$(ps -ef | grep etcd | grep -v grep | grep "name node$i" | awk '{print $2}')
+    echo $PID
+    sudo /home/zyh/criu/scripts/criu-ns \
+        dump --tree $(ps -ef | grep etcd | grep -v grep | grep "name node$i" | awk '{print $2}') \
+        -D checkpoint_folder/$i -v4 --tcp-established --file-locks -vvv -o dump.log && echo OK
+done
+
+cp -r data data_backup
+
+sudo chmod 777 -R checkpoint_folder
+
+./stop.sh
