4a301cdf9382d6a2cdfaa6487a29e3f736f68367
zouyonghao <yonghaoz1994@gmail.com>
2022-05-04 07:07:29 +0000

    ADD macro for kafka bootstrap server

--- fuzz/include/constants.h
+++ fuzz/include/constants.h
@@ -4,7 +4,9 @@
 #define MAP_SIZE_POW2 23
 #define MAP_SIZE (1u << MAP_SIZE_POW2)
 
+#ifndef BOOTSTRAP_SERVER
 #define BOOTSTRAP_SERVER "control:9092"
+#endif
 static const char *topic_code_coverage_str = "code_coverage";
 static const char *topic_order_coverage_str = "order_coverage";
 #define KAFKA_COVERAGE_TOPIC "coverage"
