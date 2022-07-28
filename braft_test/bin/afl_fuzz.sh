a2580b8410d871b0910c8d65fc90af6a7283354f
Yong-Hao Zou <yonghaoz1994@gmail.com>
2022-07-28 17:49:04 +0800

    Fix braft test

--- braft_test/bin/afl_fuzz.sh
+++ braft_test/bin/afl_fuzz.sh
@@ -1,3 +1,3 @@
 ulimit -s 81910
-../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
-    /bin/bash /home/zyh/distributed-system-test/braft_test/bin/run_test.sh
+../../build/fuzz/fuzzer -p ssR2018Ssr -b -i input -o output -M fuzzer1 -f init_random.txt \
+    /bin/bash ./run_test.sh
--- braft_test/bin/run_test.sh
+++ braft_test/bin/run_test.sh
@@ -1,3 +1,3 @@
 ./start_event_control.sh
-/home/zyh/distributed-system-test/build/braft_test/braft_test_main init_random.txt 5 > log_test 2>&1
+/home/zyh/distributed-system-test/build/braft_test/braft_test_main --node_count=5 --normal_count=5 --critic_count=3 --check_after_fuzz=true > log_test 2>&1
 ./stop.sh
