18de281cc376c5e811b67b5b71e25e8b866d1ba4
Yong-Hao Zou <yonghaoz1994@gmail.com>
2022-08-03 17:30:24 +0800

    Fix fuzz script

--- general_test/bin/afl_fuzz.sh
+++
@@ -1,1 +0,0 @@
-fuzzer -i input -o output -M fuzzer1 -f init_random.txt /bin/bash ./run_test.sh
---
+++ general_test/bin/fuzz.sh
@@ -0,0 +1,1 @@
+fuzzer -i input -o output -l licenses -M fuzzer1 -f init_random.txt /bin/bash ./run_test.sh
