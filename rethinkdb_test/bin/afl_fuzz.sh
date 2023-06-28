b34961ce95a698f745f3160362f0ac000690dcc2
zouyonghao <yonghaoz1994@gmail.com>
2023-06-28 15:24:10 +0800

    Fix rethinkdb test

--- rethinkdb_test/bin/afl_fuzz.sh
+++ rethinkdb_test/bin/afl_fuzz.sh
@@ -1,5 +1,9 @@
 ./env_init.sh
 
-../../build/fuzz/fuzzer -i input -o output -M fuzzer1 -f init_random.txt /bin/bash /home/zyh/distributed-system-test/rethinkdb_test/bin/run_test.sh
+../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
+    ../../build/rethinkdb_test/rethinkdb_test_main \
+        --node_count=5 \
+        --normal_count=5 --critic_count=3 \
+        --check_after_fuzz=true
 
 ./env_clear.sh
