7ec957fe773d669694fcf055841210a7c870dd94
zouyonghao <yonghaoz1994@gmail.com>
2023-06-29 11:28:58 +0800

    Update rethinkdb script

--- rethinkdb_test/bin/afl_fuzz.sh
+++ rethinkdb_test/bin/afl_fuzz.sh
@@ -1,9 +1,5 @@
-./env_init.sh
-
 ../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/rethinkdb_test/rethinkdb_test_main \
         --node_count=5 \
         --normal_count=5 --critic_count=3 \
         --check_after_fuzz=true
-
-./env_clear.sh
--- rethinkdb_test/bin/stop.sh
+++ rethinkdb_test/bin/stop.sh
@@ -1,3 +1,6 @@
 killall rethinkdb -9
 # killall proxy_server
 killall event_control
+
+rm -rf rethinkdb_data*
+sudo tunctl -d tap0
