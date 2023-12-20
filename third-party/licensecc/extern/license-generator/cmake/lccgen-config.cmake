57bacd5d038490576999a24461880fa552e9b7a3
zouyonghao <yonghaoz1994@gmail.com>
2023-12-20 12:57:04 +0100

    Remove license code

--- third-party/licensecc/extern/license-generator/cmake/lccgen-config.cmake
+++
@@ -1,4 +0,0 @@
-# lcc-config.cmake - package configuration file
-
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/license_generator.cmake)
--- third-party/licensecc/extern/license-generator/src/license_generator/open-license-main.cpp
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-#include "command_line-parser.hpp"
-
-int main(int argc, const char *argv[]) { return license::CommandLineParser::parseCommandLine(argc, argv); }
