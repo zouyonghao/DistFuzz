93c67eb3bbc02797b78d2e8c12715f13b3bb5d22
zouyonghao <yonghaoz1994@gmail.com>
2024-01-07 14:42:33 +0100

    update zk scripts

--- zookeeper_test/bin/afl_fuzz_traditional.sh
+++ zookeeper_test/bin/afl_fuzz_traditional.sh
@@ -1,4 +1,4 @@
-../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
+../../build/fuzz/fuzzer -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/zookeeper_test/zookeeper_test_main \
     --node_count=5 \
     --normal_count=5 --critic_count=3 \
