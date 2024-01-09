c91ebaadad95a75b166604c3ee3e28e7d9e69ffa
zouyonghao <yonghaoz1994@gmail.com>
2024-01-09 14:34:52 +0100

    update nofault scripts

---
+++ aerospike_test/bin/afl_fuzz_nofault.sh
@@ -0,0 +1,7 @@
+ulimit -n 20000
+export NO_FAULT=1
+../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
+    ../../build/aerospike_test/aerospike_test_main \
+        --node_count=5 \
+        --normal_count=5 --critic_count=3 \
+        --check_after_fuzz=true
---
+++ etcd_test/bin/afl_fuzz_nofault.sh
@@ -0,0 +1,6 @@
+export NO_FAULT=1
+../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
+    ../../build/etcd_test/etcd_test_main \
+    --node_count=5 \
+    --normal_count=5 --critic_count=3 \
+    --check_after_fuzz=true
