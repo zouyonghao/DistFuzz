770d767fdcaa20150b776ab1f9794fca2aea4a0b
Yonghao Zou <yonghaoz1994@gmail.com>
2024-01-09 16:27:08 +0100

    do not fuzz before init for aerospike

--- aerospike_test/bin/afl_fuzz.sh
+++ aerospike_test/bin/afl_fuzz.sh
@@ -5,6 +5,7 @@ sudo ifconfig tap0 127.0.1.1/24 up
 
 ../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/aerospike_test/aerospike_test_main \
+        --fuzz_before_init=false \
         --node_count=5 \
         --normal_count=5 --critic_count=3 \
         --check_after_fuzz=true
