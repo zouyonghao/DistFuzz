c3b1aced5234b378a6bf2cd7a74fb995ccaa44e4
Yonghao Zou <yonghaoz1994@gmail.com>
2024-05-20 11:33:11 +0200

    Add hdfs r/w

--- hdfs_test/include/hdfs_configuration_generator.hpp
+++ hdfs_test/include/hdfs_configuration_generator.hpp
@@ -1,5 +1,5 @@
-#ifndef ZOOKEEPER_CONFIGURATION_GENERATOR_HEADER
-#define ZOOKEEPER_CONFIGURATION_GENERATOR_HEADER
+#ifndef HDFS_CONFIGURATION_GENERATOR_HEADER
+#define HDFS_CONFIGURATION_GENERATOR_HEADER
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
@@ -8,17 +8,20 @@
 #include <operator/dst_default_client_operator.hpp>
 #include <operator/dst_default_init_operator.hpp>
 
+#define HADOOP_PATH "/home/zouyo/hadoop"
+
 #define BIN_PATH                                                                                                       \
-    "java -cp "                                                                                                        \
-    "/home/zouyo/hadoop:/home/zouyo/hadoop/share/hadoop/common/*:/home/zouyo/hadoop/share/hadoop/common/lib/*:/home/"  \
-    "zouyo/hadoop/share/hadoop/hdfs/*:/home/zouyo/hadoop/share/hadoop/hdfs/lib/*:./datanode"
+    "java -cp " HADOOP_PATH ":" HADOOP_PATH "/share/hadoop/common/*:" HADOOP_PATH                                      \
+    "/share/hadoop/common/lib/*:" HADOOP_PATH "/share/hadoop/hdfs/*:" HADOOP_PATH                                      \
+    "/share/hadoop/hdfs/lib/*:./datanode"
 
 class HdfsConfigurationGenerator : public ServerConfigurationGenerator
 {
 public:
     std::string get_configure_string(uint32_t node_id, uint32_t node_count) override
     {
-        std::string config = BIN_PATH + std::to_string(node_id + 1) + " org.apache.hadoop.hdfs.server.datanode.DataNode";
+        std::string config =
+            BIN_PATH + std::to_string(node_id + 1) + " org.apache.hadoop.hdfs.server.datanode.DataNode";
         return config;
     }
     std::string get_data_folder(uint32_t node_id) override { return "hadoop/datanode" + std::to_string(node_id + 1); }
@@ -31,4 +34,43 @@ public:
     }
 };
 
-#endif // ZOOKEEPER_CONFIGURATION_GENERATOR_HEADER
+/* The ClientConfigurationGenerator for zookeeper, the example is as follows:
+ * echo 1 > a
+ * hadoop/bin/hdfs dfs -put ./a /a
+ * hadoop/bin/hdfs dfs -cat /a
+ */
+class HdfsClientConfigurationGenerator : public ClientConfigurationGenerator
+{
+public:
+    std::string get_configure_string(OP_NAME op_name, uint32_t node_count, ...)
+    {
+        std::string configure_string = "timeout 3 " HADOOP_PATH "/bin/hdfs dfs ";
+        va_list random_nums;
+        va_start(random_nums, node_count);
+        switch (op_name)
+        {
+        case OP_READ:
+            configure_string += "-cat /a";
+            break;
+        case OP_WRITE:
+        {
+            uint32_t value = va_arg(random_nums, uint32_t);
+            boost::filesystem::ofstream a("./a");
+            a << value;
+            a.close();
+            configure_string += "-put ./a /a";
+            break;
+        }
+        case OP_CAS:
+        {
+            break;
+        }
+        default:
+            break;
+        }
+        va_end(random_nums);
+        return configure_string;
+    }
+};
+
+#endif // HDFS_CONFIGURATION_GENERATOR_HEADER
--- hdfs_test/src/operators/hdfs_client_test.cpp
+++ hdfs_test/src/operators/hdfs_client_test.cpp
@@ -1,3 +1,27 @@
 #include <hdfs_configuration_generator.hpp>
 
-REGISTER_NORMAL_OPERATOR(HDFSSleepOperator, new SimpleCommandOperator("sleep 2"));
+
+static ClientConfigurationGenerator *hdfs_client_configuration_generator =
+    new HdfsClientConfigurationGenerator();
+
+class HdfsClientOperator : public DefaultClientOperator
+{
+public:
+    HdfsClientOperator(OP_NAME op_name, ClientConfigurationGenerator *ccg) : DefaultClientOperator(op_name, ccg) {}
+
+    int64_t parse_read_result(boost::process::ipstream &pipe_stream) override
+    {
+        /* the last output should be the result of read */
+        std::string last_output;
+        std::string tmp;
+        while (pipe_stream && std::getline(pipe_stream, tmp))
+        {
+            std::cerr << tmp << "\n";
+            last_output = tmp;
+        }
+        return std::stoll(last_output);
+    }
+};
+
+REGISTER_NORMAL_OPERATOR(HdfsGet, new HdfsClientOperator(OP_READ, hdfs_client_configuration_generator));
+REGISTER_NORMAL_OPERATOR(HdfsSet, new HdfsClientOperator(OP_WRITE, hdfs_client_configuration_generator));
