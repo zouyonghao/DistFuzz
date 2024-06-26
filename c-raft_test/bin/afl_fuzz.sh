bf70c636143c26dee0b3a4da324ac2a972a5d956
Yonghao Zou <yonghaoz1994@gmail.com>
2024-06-26 16:16:33 +0200

    update c-raft

--- c-raft_test/bin/afl_fuzz.sh
+++ c-raft_test/bin/afl_fuzz.sh
@@ -1,1 +1,8 @@
-../../build/fuzz/fuzzer -i input -o output -M fuzzer1 -f init_random.txt /bin/bash /home/zyh/distributed-system-test/c-raft_test/bin/run_test.sh
+# ../../build/fuzz/fuzzer -i input -o output -M fuzzer1 -f init_random.txt \
+#     /bin/bash /home/zyh/distributed-system-test/c-raft_test/bin/run_test.sh
+
+../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
+    ../../build/braft_test/braft_test_main \
+        --node_count=5 \
+        --normal_count=5 --critic_count=3 \
+        --check_after_fuzz=true
