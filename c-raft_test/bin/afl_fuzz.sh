8634f170d2149324fc38aed696a84c6d2c33d3df
Yonghao Zou <yonghaoz1994@gmail.com>
2024-06-26 15:59:48 +0000

    reduce event count

--- c-raft_test/bin/afl_fuzz.sh
+++ c-raft_test/bin/afl_fuzz.sh
@@ -4,5 +4,5 @@
 ../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/c-raft_test/c-raft_test_main \
         --node_count=5 \
-        --normal_count=5 --critic_count=3 \
+        --normal_count=3 --critic_count=2 \
         --check_after_fuzz=true
