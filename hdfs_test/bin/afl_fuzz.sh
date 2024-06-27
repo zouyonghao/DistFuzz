93ad10b4ee474c56f7c438510848f8a63ca4c1f6
Yonghao Zou <yonghaoz1994@gmail.com>
2024-06-27 11:12:07 +0200

    hdfs: add no fault files

--- hdfs_test/bin/afl_fuzz.sh
+++ hdfs_test/bin/afl_fuzz.sh
@@ -1,4 +1,4 @@
-export NO_FAULT_FILES=xml
+export NO_FAULT_FILES=xml,html,jar
 ../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/hdfs_test/hdfs_test_main \
     --node_count=3 \
