12ad5c9ef729fa20eb2bd8ba3519cb5716079cd3
Yonghao Zou <yonghaoz1994@gmail.com>
2024-06-30 23:26:30 +0200

    c-raft: enable trace

--- c-raft_test/bin/afl_fuzz.sh
+++ c-raft_test/bin/afl_fuzz.sh
@@ -1,6 +1,6 @@
 # ../../build/fuzz/fuzzer -i input -o output -M fuzzer1 -f init_random.txt \
 #     /bin/bash /home/zyh/distributed-system-test/c-raft_test/bin/run_test.sh
-
+export LIBRAFT_TRACE=1 LIBDQLITE_TRACE=1
 ../../build/fuzz/fuzzer -b -i input -o output -M fuzzer1 -f init_random.txt \
     ../../build/c-raft_test/c-raft_test_main \
         --node_count=5 \
