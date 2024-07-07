5ffefaab0e7c437b990a1f5eb5ff96962bc7b04d
Yonghao Zou <yonghaoz1994@gmail.com>
2024-07-08 00:11:43 +0200

    craft: grace kill

--- c-raft_test/src/node_manager.cpp
+++ c-raft_test/src/node_manager.cpp
@@ -1,3 +1,4 @@
 #include <craft_configuration_generator.hpp>
+#include <craft_node_manager.hpp>
 
-REGISTER_NODE_MANAGER(CRAFT_NODE_MANAGER, new NodeManager(new CraftConfigurationGenerator()));
+REGISTER_NODE_MANAGER(CRAFT_NODE_MANAGER, new CraftNodeManager(new CraftConfigurationGenerator()));
---
+++ c-raft_test/src/operators/craft_random_grace_kill.cpp
@@ -0,0 +1,33 @@
+#include <craft_node_manager.hpp>
+#include <dst_node_manager.hpp>
+#include <dst_random.h>
+#include <operator/dst_random_kill_operator.hpp>
+
+class GraceRandomKill : public CriticalOperator
+{
+public:
+    GraceRandomKill() = default;
+    bool _do() override;
+};
+
+bool GraceRandomKill::_do()
+{
+    NodeManager *nm = SingletonRegistry<NodeManager>::getItem();
+    CraftNodeManager *dnm = dynamic_cast<CraftNodeManager *>(nm);
+    std::vector<const NodeInfo *> alive_nodes;
+    for (const NodeInfo &ni : dnm->get_node_processes())
+    {
+        if (ni.should_alive)
+        {
+            alive_nodes.push_back(&ni);
+        }
+    }
+    if (alive_nodes.empty())
+    {
+        std::cerr << "all nodes are stopped.\n";
+        return true;
+    }
+    return dnm->grace_stop_node(alive_nodes[__dst_get_random_uint8_t() % alive_nodes.size()]->node_id);
+}
+
+REGISTER_CRITICAL_OPERATOR(GraceRandomKill, new GraceRandomKill);
